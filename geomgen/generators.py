5004710cd8a52e8d969286b9954f55f255d926cc
u212119 <juerg.kraehenbuehl@sbb.ch>
2022-11-19 17:24:06 +0100

    add noise

--- geomgen/generators.py
+++ geomgen/generators.py
@@ -17,8 +17,15 @@ def gen_shapes(generator=np.random.default_rng(100), num_shapes=25,max_radius=1/
       p[4] = p[4]*2*np.pi/p[0]
   return params
 
+def gen_noise(generator=np.random.default_rng(100), num_noise=700, max_line=1/30):
+  params = generator.random((num_noise,4))
+  params[:,0:2] = params[:,0:2]*(1-2*max_line)+max_line
+  params[:,2] = params[:,2]*max_line
+  params[:,3] = params[:,3]*2*np.pi
+  return params
 
-def gen_image(shapes, im_size=160,lnw = 0.2):
+
+def gen_image(shapes, noise = None, im_size=160, lw_shape=0.15, lw_noise=0.1):
   sha = shapes.copy()
   sha[:,1:4] = sha[:,1:4]*im_size
   fig, ax = plt.subplots(figsize=(1,1), dpi=im_size)
@@ -29,10 +36,17 @@ def gen_image(shapes, im_size=160,lnw = 0.2):
   plt.subplots_adjust(bottom=0.0, left=0.0, right=1.0, top=1.0)
   for s in sha:
     if s[0] < 3:
-      ax.add_patch(matplotlib.patches.Circle(s[1:3], radius=s[3], lw=lnw, fc='b', fill=False))
+      ax.add_patch(matplotlib.patches.Circle(s[1:3], radius=s[3], lw=lw_shape, fc='b', fill=False))
     else:
-      ax.add_patch(matplotlib.patches.RegularPolygon(s[1:3],numVertices=int(s[0]),radius=s[3],orientation=s[4],lw=lnw,fc='b',fill=False))
+      ax.add_patch(matplotlib.patches.RegularPolygon(s[1:3],numVertices=int(s[0]),radius=s[3],orientation=s[4],lw=lw_shape,fc='b',fill=False))
+  if not noise is None:
+    nse = noise.copy()
+    nse[:,0:3] = nse[:,0:3]*im_size
+    for n in nse:
+      ax.add_line(matplotlib.lines.Line2D((n[0],n[0]+[np.cos(n[3])*n[2]]),(n[1],n[1]+[np.sin(n[3])*n[2]]),lw=lw_noise,c='k'))
+  else:
+    nse = None  
   fig.set(figwidth=1, figheight=1, dpi=im_size)
   fig.canvas.draw()
   img = np.array(fig.canvas.renderer.buffer_rgba())
-  return (img[:,:,0],sha) 
+  return (img[:,:,0],sha,nse) 
