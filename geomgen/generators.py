3d9513859f2acec9b89f385f8f7321548e2ed925
jkcas22@gmail.com <jkcas22@gmail.com>
2022-11-20 23:49:55 +0100

    image for each shape

--- geomgen/generators.py
+++ geomgen/generators.py
@@ -4,12 +4,13 @@ import matplotlib
 import matplotlib.pyplot as plt
 from PIL import Image
 
-def gen_shapes(generator=np.random.default_rng(100), num_shapes=25,max_radius=1/20,min_radius=1/40):
+
+def gen_shapes(rnd=np.random.default_rng(100), num_shapes=25,max_radius=1/20,min_radius=1/40):
   params = np.zeros((num_shapes,5))
-  params[:,0] = generator.integers(2,5,size=num_shapes)
-  params[:,1:5] = generator.random((num_shapes,4))
+  params[:,0] = rnd.integers(2,5,size=num_shapes)
+  params[:,1:5] = rnd.random((num_shapes,4))
+  params[:,3] = min_radius+params[:,3]*(max_radius-min_radius)
   for p in params:
-    p[3] = min_radius+p[3]*(max_radius-min_radius)
     p[1:3] = p[1:3]*(1-2*p[3])+p[3]
     if p[0] < 3:
       p[4] = 0
@@ -17,17 +18,20 @@ def gen_shapes(generator=np.random.default_rng(100), num_shapes=25,max_radius=1/
       p[4] = p[4]*2*np.pi/p[0]
   return params
 
-def gen_noise(generator=np.random.default_rng(100), num_noise=700, max_line=1/30):
-  params = generator.random((num_noise,4))
-  params[:,0:2] = params[:,0:2]*(1-2*max_line)+max_line
-  params[:,2] = params[:,2]*max_line
+
+def gen_noise(rnd=np.random.default_rng(100), num_noise=500, max_line=1/20, min_line=1/80):
+  params = rnd.random((num_noise,4))
+  params[:,2] = min_line+params[:,2]*(max_line-min_line)
   params[:,3] = params[:,3]*2*np.pi
+  for p in params:
+    p[0:2] = p[0:2]*(1-2*p[2])+p[2]
   return params
 
 
-def gen_image(shapes, noise = None, im_size=160, lw_shape=0.15, lw_noise=0.1):
-  sha = shapes.copy()
+def gen_image(shapes, noise = None, rnd=np.random.default_rng(100), im_size=160, max_lw=0.15, min_lw=0.1):
+  sha = np.c_[shapes.copy(),rnd.random(len(shapes))]
   sha[:,1:4] = sha[:,1:4]*im_size
+  sha[:,5] = min_lw+sha[:,5]*(max_lw-min_lw)
   fig, ax = plt.subplots(figsize=(1,1), dpi=im_size)
   plt.axis('scaled')
   plt.axis('off')
@@ -36,14 +40,15 @@ def gen_image(shapes, noise = None, im_size=160, lw_shape=0.15, lw_noise=0.1):
   plt.subplots_adjust(bottom=0.0, left=0.0, right=1.0, top=1.0)
   for s in sha:
     if s[0] < 3:
-      ax.add_patch(matplotlib.patches.Circle(s[1:3], radius=s[3], lw=lw_shape, fc='b', fill=False))
+      ax.add_patch(matplotlib.patches.Circle(s[1:3], radius=s[3], lw=s[5], fc='b', fill=False))
     else:
-      ax.add_patch(matplotlib.patches.RegularPolygon(s[1:3],numVertices=int(s[0]),radius=s[3],orientation=s[4],lw=lw_shape,fc='b',fill=False))
+      ax.add_patch(matplotlib.patches.RegularPolygon(s[1:3],numVertices=int(s[0]),radius=s[3],orientation=s[4],lw=s[5],fc='b',fill=False))
   if not noise is None:
-    nse = noise.copy()
+    nse = np.c_[noise.copy(),rnd.random(len(noise))]
     nse[:,0:3] = nse[:,0:3]*im_size
+    nse[:,4] = min_lw+nse[:,4]*(max_lw-min_lw)
     for n in nse:
-      ax.add_line(matplotlib.lines.Line2D((n[0],n[0]+[np.cos(n[3])*n[2]]),(n[1],n[1]+[np.sin(n[3])*n[2]]),lw=lw_noise,c='k'))
+      ax.add_line(matplotlib.lines.Line2D((n[0],n[0]+[np.cos(n[3])*n[2]]),(n[1],n[1]+[np.sin(n[3])*n[2]]),lw=n[4],c='k'))
   else:
     nse = None  
   fig.set(figwidth=1, figheight=1, dpi=im_size)
